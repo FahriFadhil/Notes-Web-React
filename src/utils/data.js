8a397e96bcf30cd44bbfd393e5e42740b1a1b0df
diasdev15 <utsmand91@gmail.com>
2022-06-17 05:49:32 +0700

    seperate createNoteObject function

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,2 +1,13 @@
+// Additional to Update Data in Local Storage
 
+function createNoteObject({ title, content }) {
+    return {
+        id: +new Date(),
+        title: title,
+        content: content,
+        createdAt: new Date().toISOString(),
+        archived: false
+    }
+}
 
+export { createNoteObject }
